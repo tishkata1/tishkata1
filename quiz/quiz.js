31eb3c0e58f5644948daf92927aa8150d4f62eb4
tishkata1 <tishkata11@gmail.com>
2017-01-10 22:31:54 +0200

    Add files via upload

---
+++ quiz/quiz.js
@@ -0,0 +1,98 @@
+
+
+/** Quiz controller **/
+
+function Quiz(questions){
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function(){
+    return this.questions[this.questionIndex];
+};
+
+Quiz.prototype.isEnded = function(){
+    return this.questions.length === this.questionIndex;
+};
+
+//correct or not answer
+
+Quiz.prototype.guess = function(answer){
+    if(this.getQuestionIndex().correctAnswer(answer)){
+        this.score++;
+    }
+    this.questionIndex++;
+};
+
+
+
+/** Question **/
+
+function Question(text, choices, answer){
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+
+}
+
+Question.prototype.correctAnswer = function(choice){
+    return choice === this.answer;
+};
+
+
+/**                 APP           **/
+
+function populate() {
+    if (quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        //show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        //show choices
+        var choices = quiz.getQuestionIndex().choices;
+        for (var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+        showProgress();
+    }
+}
+
+    function guess(id, guess){
+        var button = document.getElementById(id);
+        button.onclick = function(){
+            quiz.guess(guess);
+            populate();
+        }
+    }
+
+
+// change the progress of questions
+function showProgress(){
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+}
+
+function showScores(){
+    var gameOver = "<h1>Result</h1>";
+        gameOver += "<h2 id='score'> Your score: " + quiz.score + "</h2>";
+        var element = document.getElementById("wrapper");
+        element.innerHTML = gameOver;
+}
+
+var questions=[
+    new Question("What is my name bro?", ["Tishkata", "Lebron", "maaf", "ivan"], "Tishkata"),
+    new Question("What is my height?", ["1.90", "1.72", "1.80", "1.50"], "1.72"),
+    new Question("My favourite team?", ["CSKA", "Litex", "leWCki", "nsdnos"], "CSKA"),
+    new Question("My hometown?", ["Mezdra", "Sofia", "massf", "Vratsa"], "Vratsa")
+];
+
+var quiz = new Quiz(questions);
+
+populate();
